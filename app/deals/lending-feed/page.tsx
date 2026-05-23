4df1c761bd5bb6f511ef6472b67d53c1db9eea4a
AIForceMultiplierData <founder@forcemultiplierdata.com>
2026-05-23 18:10:22 +0530

    Add files via upload

---
+++ app/deals/lending-feed/page.tsx
@@ -0,0 +1,11 @@
+"use client"
+
+import { LenderFeed } from "@/app/deals/lender-feed"
+
+export default function LenderFeedPage() {
+  return (
+    <div className="min-h-screen bg-slate-950 px-4 pb-32 pt-4">
+      <LenderFeed />
+    </div>
+  )
+}
